c43a7d9c6e61632cbd24fa9ecd4c5f3e714dd3e1
Lorenzo Peve <lpeve01@gmail.com>
2024-10-12 17:44:59 -0500

    Update test_openai.py

--- tests/test_openai.py
+++ tests/test_openai.py
@@ -1,28 +1,41 @@
 import pytest
 from src import openai
 
-def test_num_tokens_from_string():
+
+def test_get_num_tokens_from_string():
     test_string = "Hello, world!"
-    assert openai.num_tokens_from_string(test_string) == 4
+    assert openai.get_num_tokens_from_string(test_string) == 4
+
 
 def test_get_tokens_from_string():
     test_string = "Hello, world!"
     assert openai.get_tokens_from_string(test_string) == [9906, 11, 1917, 0]
 
+
 def test_get_string_from_tokens():
-    tokens = [9906, 11, 1917, 0]  # Adjust based on your tokenizer
+    tokens = [9906, 11, 1917, 0]
     expected_string = "Hello, world!"
     assert openai.get_string_from_tokens(tokens) == expected_string
 
+
 def test_check_token_limit():
-    test_string = "Hello, world!"*5000
-    openai.TOKEN_LIMIT = 5  # Set a limit for testing
+    test_string = "Hello, world!" * 5000
+    openai.TOKEN_LIMIT = 5
     with pytest.raises(ValueError):
         openai.check_token_limit(test_string)
+
 
 def test_get_embedding():
 
     test_string = "Hello, world!"
+    embeddings = openai.get_embedding(test_string)
+    assert isinstance(embeddings, list)
+    assert len(embeddings) == 1536
+
+
+def test_get_embedding_2():
+
+    test_string = "What are key features of a good data engineering team?"
     embeddings = openai.get_embedding(test_string)
     assert isinstance(embeddings, list)
     assert len(embeddings) == 1536
