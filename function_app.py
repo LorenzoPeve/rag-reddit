0b5fdd607aa175f27c7557a03ebbe2ce6b56bcd0
Lorenzo Peve <lpeve01@gmail.com>
2024-10-26 22:18:38 -0500

    Update function_app.py

--- function_app.py
+++ function_app.py
@@ -3,14 +3,22 @@ from dotenv import load_dotenv
 import logging
 import os
 from sqlalchemy.orm import Session
+from time import time
 
 from src import reddit, db
 from src.db import RedditPosts
+from concurrent.futures import ThreadPoolExecutor
 
-load_dotenv(".env.dev", override=True)
+load_dotenv(".env.production", override=True)
 
-CHUNK_SIZE = int(os.getenv('CHUNK_SIZE'))
-CHUNK_OVERLAP = int(os.getenv('CHUNK_OVERLAP'))
+CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))
+CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP"))
+
+def delete_post(post_id: str):
+    logging.info(f'Deleting existing post: {post_id}')
+    with Session(db.get_db_engine()) as session:
+        session.query(RedditPosts).filter_by(id=post_id).delete()
+        session.commit()
 
 def insert_reddit_post_and_comments(p: dict) -> None:
     """
@@ -18,7 +26,7 @@ def insert_reddit_post_and_comments(p: dict) -> None:
     tables.
 
     If the post already exists and has been modified, the existing post is
-    deleted and a new post is inserted. Otherwise, the post is skipped.    
+    deleted and a new post is inserted. Otherwise, the post is skipped.
     """
     with Session(db.get_db_engine()) as session:
         db_post = session.query(RedditPosts).filter_by(id=p["id"]).first()
@@ -26,39 +34,68 @@ def insert_reddit_post_and_comments(p: dict) -> None:
     if db_post is None:
         logging.info(f'Inserting Reddit post: {p["id"]}')
         db.insert_reddit_post(p)
-        db.insert_documents_from_comments_body(p['id'], CHUNK_SIZE, CHUNK_OVERLAP)
+        db.insert_documents_from_comments_body(p["id"], CHUNK_SIZE, CHUNK_OVERLAP)
     else:
         logging.info(f'Reddit post already exists: {p["id"]}')
-        
-        if db.is_post_modified(p['id']):
+
+        if not db.is_post_modified(p["id"]):
+            logging.info(f'Skipping Reddit post: {p["id"]}. Already up-to-date.')
+        else:
             logging.info(f'Reddit post has been modified: {p["id"]}')
-            
-            logging.info(f'Deleting existing post: {p["id"]}')
-            with Session(db.get_db_engine()) as session:
-                session.query(RedditPosts).filter_by(id=p["id"]).delete()
-                session.commit()
+            delete_post(p["id"])
 
             logging.info(f'Inserting Reddit post: {p["id"]}')
             db.insert_reddit_post(p)
-            db.insert_documents_from_comments_body(p['id'], CHUNK_SIZE, CHUNK_OVERLAP)
+            db.insert_documents_from_comments_body(p["id"], CHUNK_SIZE, CHUNK_OVERLAP)
+
 
 def insert_reddit_posts(posts: list[dict]):
     """Inserts a list of Reddit posts into the `posts` and `documents` tables."""
-    for p in posts:
-        logging.info('Processing Reddit post: %s', p["permalink"])
+
+    def process_post(p):
+        logging.info(f'Processing Reddit post: {p["id"]}')
 
         if p["link_flair_text"] == "Meme":
-            logging.info(f'Skipping Reddit post. Reason: Meme')
-            continue
+            logging.info(f"Skipping Reddit post. Reason: Meme")
+            return
+        try:
+            insert_reddit_post_and_comments(p)
+        except Exception as e:
+            logging.error(f"Error processing post: {p['id']}. Error: {e}")
 
-        insert_reddit_post_and_comments(p)
+    with ThreadPoolExecutor(max_workers=3) as executor:
+        futures = [executor.submit(process_post, p) for p in posts]
+
 
 app = func.FunctionApp()
 
-@app.schedule(schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True,
-              use_monitor=False) 
-def get_reddit_comments(myTimer: func.TimerRequest) -> None:
-    if myTimer.past_due:
-        logging.info('The timer is past due!')
 
-    logging.info('Python timer trigger function executed.')
+@app.schedule(
+    schedule="0 0 * * * *", arg_name="myTimer", run_on_startup=True, use_monitor=False
+)
+def get_reddit_comments(myTimer: func.TimerRequest) -> None:
+    logging.info("Function starting")
+
+    logging.info("Fetching top posts from r/dataengineering 1")
+    posts = reddit.get_top_posts("dataengineering", limit=100, t="month")
+    insert_reddit_posts(posts)
+
+    time.sleep(10)
+    logging.info("Fetching top posts from r/dataengineering 2")
+    after = "t3_" + posts[-1]["id"]
+    posts = reddit.get_top_posts("dataengineering", limit=100, t="month", after=after)
+    insert_reddit_posts(posts)
+
+    time.sleep(10)
+    logging.info("Fetching top posts from r/dataengineering 3")
+    after = "t3_" + posts[-1]["id"]
+    posts = reddit.get_top_posts("dataengineering", limit=100, t="month", after=after)
+    insert_reddit_posts(posts)
+
+    time.sleep(10)
+    logging.info("Fetching top posts from r/dataengineering 4")
+    after = "t3_" + posts[-1]["id"]
+    posts = reddit.get_top_posts("dataengineering", limit=100, t="month", after=after)
+    insert_reddit_posts(posts)
+
+    logging.info("Function completed")
