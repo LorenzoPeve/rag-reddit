8a9fd3d54594c8662d72e51ab57f25b01f052191
Lorenzo Peve <lpeve01@gmail.com>
2024-10-31 21:20:48 -0500

    Update init_etl.py

--- etl/init_etl.py
+++ etl/init_etl.py
@@ -1,40 +1,9 @@
-from dotenv import load_dotenv
-import json
 import os
-from sqlalchemy import create_engine, text
-from sqlalchemy.orm import Session
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
-from src import reddit, db, openai
-from src.db import RedditPosts
-
-ENVIRONMENT = os.getenv('ENVIRONMENT', 'dev')
-
-load_dotenv(f".env.{ENVIRONMENT}", override=True)
-
-CHUNK_SIZE = int(os.getenv('CHUNK_SIZE'))
-CHUNK_OVERLAP = int(os.getenv('CHUNK_OVERLAP'))
-
-
-def load_posts():
-
-    posts = reddit.get_top_posts('dataengineering', limit=100, t='month')
-
-    for p in posts:
-
-        print(p['permalink'])
-
-        if p['link_flair_text'] == 'Meme':
-            continue
-
-        db.insert_reddit_post(p)
-        db.insert_documents_from_comments_body(p['id'], CHUNK_SIZE, CHUNK_OVERLAP)
-
-      
+from src import db 
 
 if __name__ == "__main__":
-
     db.init_schema()
-    load_posts()
