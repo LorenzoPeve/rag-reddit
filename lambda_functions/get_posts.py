d917efa38d96e4e1d5b07c58ae6de3a5970ff73c
Lorenzo Peve <lpeve01@gmail.com>
2024-11-02 11:43:09 -0500

    change to print stetements to show log

--- lambda_functions/get_posts.py
+++ lambda_functions/get_posts.py
@@ -1,6 +1,4 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from dotenv import load_dotenv
-import logging
 import os
 from sqlalchemy.orm import Session
 import sys
@@ -11,19 +9,13 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from src import reddit, db
 from src.db import RedditPosts
 
-load_dotenv(".env.production", override=True)
-
-logging.basicConfig(
-    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
 
 CHUNK_SIZE = int(os.getenv("CHUNK_SIZE"))
 CHUNK_OVERLAP = int(os.getenv("CHUNK_OVERLAP"))
 
 
 def delete_post(post_id: str):
-    logging.info(f"Deleting existing post: {post_id}")
+    print(f"Deleting existing post: {post_id}")
     with Session(db.engine) as session:
         session.query(RedditPosts).filter_by(id=post_id).delete()
         session.commit()
@@ -41,19 +33,19 @@ def insert_reddit_post_and_comments(p: dict) -> None:
         db_post = session.query(RedditPosts).filter_by(id=p["id"]).first()
 
     if db_post is None:
-        logging.info(f'Inserting Reddit post: {p["id"]}')
+        print(f'Inserting Reddit post: {p["id"]}')
         db.insert_reddit_post(p)
         db.insert_documents_from_comments_body(p["id"], CHUNK_SIZE, CHUNK_OVERLAP)
     else:
-        logging.info(f'Reddit post already exists: {p["id"]}')
+        print(f'Reddit post already exists: {p["id"]}')
 
         if not db.is_post_modified(p["id"]):
-            logging.info(f'Skipping Reddit post: {p["id"]}. Already up-to-date.')
+            print(f'Skipping Reddit post: {p["id"]}. Already up-to-date.')
         else:
-            logging.info(f'Reddit post has been modified: {p["id"]}')
+            print(f'Reddit post has been modified: {p["id"]}')
             delete_post(p["id"])
 
-            logging.info(f'Inserting Reddit post: {p["id"]}')
+            print(f'Inserting Reddit post: {p["id"]}')
             db.insert_reddit_post(p)
             db.insert_documents_from_comments_body(p["id"], CHUNK_SIZE, CHUNK_OVERLAP)
 
@@ -62,15 +54,15 @@ def insert_reddit_posts(posts: list[dict]):
     """Inserts a list of Reddit posts into the `posts` and `documents` tables."""
 
     def process_post(p):
-        logging.info(f'Processing Reddit post: {p["id"]}')
+        print(f'Processing Reddit post: {p["id"]}')
 
         if p["link_flair_text"] == "Meme":
-            logging.info(f"Skipping Reddit post. Reason: Meme")
+            print(f"Skipping Reddit post. Reason: Meme")
             return
         try:
             insert_reddit_post_and_comments(p)
         except Exception as e:
-            logging.error(f"Error processing post: {p['id']}. Error: {e}")
+            print(f"Error processing post: {p['id']}. Error: {e}")
 
     with ThreadPoolExecutor(max_workers=10) as executor:
         futures = [executor.submit(process_post, p) for p in posts]
@@ -80,8 +72,8 @@ def insert_reddit_posts(posts: list[dict]):
 
 
 def lambda_handler(event, context):
-    logging.info("Lambda function starting")
-    logging.info("Function starting")
+    print("Lambda function starting")
+    print("Function starting")
 
     iterations = event["iterations"]
     t = event["t"]
@@ -90,16 +82,18 @@ def lambda_handler(event, context):
     retrieved = 0
     after = None
     for i in range(iterations):
-        logging.info(f"Fetching top posts from r/dataengineering {i}")
+        print(f"Fetching top posts from r/dataengineering {i}")
         posts = reddit.get_top_posts("dataengineering", limit=n, t=t, after=after)
         after = "t3_" + posts[-1]["id"]
         insert_reddit_posts(posts)
         retrieved += len(posts)
         time.sleep(5)
 
-    logging.info("Function completed")
-    return f"Retrieved best {retrieved} posts for the {t}."
+    print("Function completed")
+    msg = f"Retrieved best {retrieved} posts for the {t}."
+    print(msg)
+    return msg
 
 
 if __name__ == "__main__":
-    print(lambda_handler({"t": "year", "iterations": 1, "n": 10}, None))
+    print(lambda_handler({"t": "year", "iterations": 6, "n": 100}, None))
