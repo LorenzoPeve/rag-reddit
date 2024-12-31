60eb1efc596054e5d897d2579acd234a8b32b807
Lorenzo Peve <71228480+LorenzoPeve@users.noreply.github.com>
2024-12-31 16:00:55 -0600

    Update init_schema.sql

--- docker/db_scripts/init_schema.sql
+++ docker/db_scripts/init_schema.sql
@@ -1,40 +1,44 @@
--- This script is run when the database container is started
--- Drop the public schema and create a new one
-DROP SCHEMA IF EXISTS public CASCADE;
-CREATE SCHEMA public;
-CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA public;
+DO $$ 
+BEGIN
 
--- Create the posts table
-CREATE TABLE posts (
-    id VARCHAR(32) NOT NULL, 
-    title VARCHAR NOT NULL, 
-    description VARCHAR, 
-    score INTEGER NOT NULL, 
-    upvotes INTEGER NOT NULL, 
-    downvotes INTEGER NOT NULL, 
-    tag VARCHAR(30), 
-    num_comments INTEGER NOT NULL, 
-    permalink VARCHAR NOT NULL, 
-    content_hash VARCHAR(32) NOT NULL, 
-    created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, 
-    last_updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, 
-    PRIMARY KEY (id)
-);
+    IF NOT EXISTS (SELECT FROM pg_tables WHERE schemaname = 'public' AND tablename = 'posts') THEN
 
--- Create the comments table
-CREATE TABLE documents (
-    id VARCHAR(32) NOT NULL, 
-    post_id VARCHAR(32) NOT NULL, 
-    chunk_id INTEGER NOT NULL, 
-    content VARCHAR NOT NULL, 
-    embedding VECTOR(1536), 
-    PRIMARY KEY (id), 
-    FOREIGN KEY(post_id) REFERENCES posts (id) ON DELETE CASCADE
-);
+        -- Run initialization only if table doesn't exist
+        CREATE EXTENSION IF NOT EXISTS vector WITH SCHEMA public;
 
-ALTER TABLE documents
-ADD COLUMN content_ts_vector tsvector
-GENERATED ALWAYS AS (to_tsvector('english', content)) STORED;
+        -- Create the posts table
+        CREATE TABLE posts (
+            id VARCHAR(32) NOT NULL, 
+            title VARCHAR NOT NULL, 
+            description VARCHAR, 
+            score INTEGER NOT NULL, 
+            upvotes INTEGER NOT NULL, 
+            downvotes INTEGER NOT NULL, 
+            tag VARCHAR(30), 
+            num_comments INTEGER NOT NULL, 
+            permalink VARCHAR NOT NULL, 
+            content_hash VARCHAR(32) NOT NULL, 
+            created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, 
+            last_updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL, 
+            PRIMARY KEY (id)
+        );
 
-CREATE INDEX content_ts_vector_idx ON documents USING GIN (content_ts_vector);
-CREATE INDEX embedding_idx ON documents USING hnsw (embedding vector_cosine_ops);
+        -- Create the comments table
+        CREATE TABLE documents (
+            id VARCHAR(32) NOT NULL, 
+            post_id VARCHAR(32) NOT NULL, 
+            chunk_id INTEGER NOT NULL, 
+            content VARCHAR NOT NULL, 
+            embedding VECTOR(1536), 
+            PRIMARY KEY (id), 
+            FOREIGN KEY(post_id) REFERENCES posts (id) ON DELETE CASCADE
+        );
+
+        ALTER TABLE documents
+        ADD COLUMN content_ts_vector tsvector
+        GENERATED ALWAYS AS (to_tsvector('english', content)) STORED;
+
+        CREATE INDEX content_ts_vector_idx ON documents USING GIN (content_ts_vector);
+        CREATE INDEX embedding_idx ON documents USING hnsw (embedding vector_cosine_ops);
+    END IF;
+END $$;
