fab9f79f237b63fbb4f29bb9feb73b6e38335898
Lorenzo Peve <71228480+LorenzoPeve@users.noreply.github.com>
2025-01-07 18:30:03 -0600

    init etl and init data

--- etl/init_etl.py
+++ etl/init_etl.py
@@ -1,4 +1,3 @@
-from src import reddit, db, rag
 from datetime import datetime
 import json
 import os
@@ -7,6 +6,7 @@ import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 
+from src import reddit, db, rag
 
 def get_posts(n: int):
     posts = reddit.get_top_posts("dataengineering", limit=n, t="all")
@@ -17,14 +17,15 @@ def get_posts(n: int):
             continue
         comments = reddit.get_all_comments_in_post(post["id"])
         p['comments'] = comments
-        p['content_hash'] = db.get_content_hash(
-            p["title"], p["description"], comments)
+        p['content_hash'] = db.get_content_hash(p["title"], p["description"], comments)
         out.append(p)
     return out
 
 
-def get_chunks_with_embeddings(post: dict):
-
+def get_chunks_with_embeddings(post: dict) -> dict:
+    """
+    Returns a dictionary containing the required attributes for a `db.Documents` object.
+    """
     assert 'title' in post
     assert 'description' in post
     assert 'comments' in post
@@ -45,20 +46,72 @@ def get_chunks_with_embeddings(post: dict):
 
         # Prepend the title to all chunks except the first one
         if chunk_id != 1:
-            chunk_content = f"{post.title}\n{chunk_content}"
+            chunk_content = f"{post['title']}\n{chunk_content}"
 
     return dict(
-        id=f"{post['post_id']}_{chunk_id}",
-        post_id=post['post_id'],
+        id=f"{post['id']}_{chunk_id}",
+        post_id=post['id'],
         chunk_id=chunk_id,
         content=chunk_content,
-        embedding=rag.llm_client.get_embedding(chunk_content),
+        embedding=rag.ThrottledOpenAI().get_embedding(chunk_content),
     )
 
 
 if __name__ == '__main__':
 
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    data_dir = os.path.join(current_dir, "data")
+
+    if 'posts_dev.json' in os.listdir(data_dir) and 'chunks_dev.json' in os.listdir(data_dir):
+        print("Data already loaded.")
+        sys.exit(0)
+
     db.init_schema()
-    posts = get_posts(15)
+
+    # Get posts ###############################################################
+    posts = get_posts(200)
     with open("posts_dev.json", "w") as f:
         json.dump(posts, f, indent=4)
+
+    # Load posts to DB
+    for i, post in enumerate(posts):
+        print(f'Loading Post {i+1} of {len(posts)}')
+        with Session(db.engine) as session:
+            post = db.RedditPosts(
+                id=post["id"],
+                title=post["title"],
+                description=post["description"],
+                score=post["score"],
+                upvotes=post["upvotes"],
+                downvotes=post["downvotes"],
+                tag=post["tag"],
+                num_comments=post["num_comments"],
+                permalink=post["permalink"],
+                content_hash=post["content_hash"],
+                created_at=datetime.fromtimestamp(post["created"]),
+                last_updated_at=datetime(2025, 1, 1)
+            )
+            session.add(post)
+            session.commit()
+
+    # Get chunks with embeddings ##############################################
+    chunks = []
+    for post in posts:
+        chunks.append(get_chunks_with_embeddings(post))
+
+    with open("chunks_dev.json", "w") as f:
+        json.dump(chunks, f, indent=4)
+
+    # Load chunks to DB
+    for i, chunk in enumerate(chunks):
+        print(f'Loading Chunk {i+1} of {len(chunks)}')
+        with Session(db.engine) as session:
+            chunk = db.Documents(
+                id=chunk["id"],
+                post_id=chunk["post_id"],
+                chunk_id=chunk["chunk_id"],
+                content=chunk["content"],
+                embedding=chunk["embedding"],
+            )
+            session.add(chunk)
+            session.commit()
